0f002b85cb1577aee8fdcb8d2390bbd21d8bcf29
frank shao <fshao@novus.com>
2017-08-31 15:30:25 -0400

    Adding type class for operator map for type configurable parser

--- src/main/scala/io/github/facaiy/math/expression/MathExp.scala
+++ src/main/scala/io/github/facaiy/math/expression/MathExp.scala
@@ -1,6 +1,8 @@
 package io.github.facaiy.math.expression
 
+import io.github.facaiy.math.expression.Operations.Operators
 import io.github.facaiy.math.expression.compiler.MathExpCompiler
+import scala.reflect.ClassTag
 
 /**
  * Created by facai on 6/19/17.
@@ -10,9 +12,10 @@ case class MathExpScannerError(msg: String) extends MathExpError
 case class MathExpParserError(msg: String) extends MathExpError
 
 object MathExp {
-  def parse(s: String): Expression[String => Double, Double] =
+  def parse[T: ClassTag: Operators](s: String): Expression[String => T, T] = {
     MathExpCompiler(s) match {
-      case Right(ts) => Expression.toExpression(ts)
+      case Right(ts) => Expression.toExpression[T](ts)
       case Left(e) => throw new IllegalArgumentException(e.toString)
     }
+  }
 }
